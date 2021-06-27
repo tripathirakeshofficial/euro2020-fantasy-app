148fc7fd387252dd23b5beb3afdc9c9242c060e6
tripathirakeshofficial <rakeshtripathi408@gmail.com>
2021-06-27 14:05:44 +0530

    ADD: Field view

---
+++ components/Field.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { ImageBackground, View, Text } from "react-native";
+
+import field from "../assets/images/field.jpg";
+import FieldPlayer from './FieldPlayer';
+
+const players: { [key: string]: null[] } = {
+  FWD: [null, null, null],
+  MID: [null, null, null],
+  DEF: [null, null, null, null],
+  GK: [null],
+};
+
+const Field = () => {
+  return (
+    <ImageBackground
+      source={field}
+      resizeMode="contain"
+      style={{
+        width: "100%",
+        aspectRatio: 2 / 3,
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
+      {Object.keys(players).map((position) => (
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around",
+            width: "100%",
+          }}
+        >
+          {players[position].map((player) => (
+            <FieldPlayer player={player} position={position} />
+          ))}
+        </View>
+      ))}
+    </ImageBackground>
+  );
+};
+
+export default Field;
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,37 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet, SafeAreaView, Pressable, Text } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import Field from "./../components/Field";
 
 export default function TabOneScreen() {
+  const viewPlayers = () => {
+    console.log("view players");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Field />
+      <Pressable onPress={viewPlayers} style={styles.buttonContainer}>
+        <Text>View Players</Text>
+      </Pressable>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  buttonContainer: {
+    backgroundColor: "orange",
+    width: "90%",
+    margin: 20,
+    padding: 10,
+    alignItems: "center",
+    borderRadius: 50,
+    marginTop: "auto",
   },
 });
