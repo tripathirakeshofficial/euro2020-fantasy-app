97981c04bdec54fefdeda82e1b77e9d93f35dc96
tripathirakeshofficial <rakeshtripathi408@gmail.com>
2021-06-30 13:18:48 +0530

    ADD: Teamstats and bottom sheet

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,20 +1,36 @@
-import * as React from "react";
-import { StyleSheet, SafeAreaView, Pressable, Text } from "react-native";
+import React, { useRef } from "react";
+import { StyleSheet, SafeAreaView, Pressable, Text, View } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import Field from "./../components/Field";
+import TeamStats from "./../components/TeamStats";
+
+import BottomSheet from "@gorhom/bottom-sheet";
 
 export default function TabOneScreen() {
+  const playerBottomSheet = useRef<BottomSheet>(null);
+
   const viewPlayers = () => {
-    console.log("view players");
+    playerBottomSheet.current?.expand();
   };
+
+  const snapPoints = [0, "50%"];
 
   return (
     <SafeAreaView style={styles.container}>
+      <TeamStats />
+
       <Field />
+
       <Pressable onPress={viewPlayers} style={styles.buttonContainer}>
         <Text>View Players</Text>
       </Pressable>
+
+      <BottomSheet ref={playerBottomSheet} index={0} snapPoints={snapPoints}>
+        <View style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </View>
+      </BottomSheet>
     </SafeAreaView>
   );
 }
@@ -24,6 +40,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     paddingTop: Platform.OS === "android" ? 25 : 0,
+    backgroundColor: "#72cc5e",
   },
   buttonContainer: {
     backgroundColor: "orange",
@@ -34,4 +51,5 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     marginTop: "auto",
   },
+  contentContainer: {},
 });
